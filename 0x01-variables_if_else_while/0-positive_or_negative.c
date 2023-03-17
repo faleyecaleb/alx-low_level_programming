42442ea8cc204196236eb2bd7aaca30095af2e95
faleyecaleb <faleyecaleb@gmail.com>
2023-03-17 15:50:35 -0700

    Just created a directory and added a c file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - This is my root function and it returns 0
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("%d%", n);
+	if(n == 0)
+	{
+		printf("%d is zero", n/n);
+	}
+	else if(n > 0)
+	{
+		printf("%d is positive", n/n);
+	}
+	else
+	{
+		printf("%d is negative", n/n);
+	}
+	return (0);
+}
